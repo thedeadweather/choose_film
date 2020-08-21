2143937a05a7498088aac0744aa0f82767f44c43
Alex Kln <kalininmalinin@gmail.com>
2020-08-21 14:50:33 +0300

    edit readme

--- spec/film_spec.rb
+++ spec/film_spec.rb
@@ -4,9 +4,9 @@ describe Film do
   describe '::new' do
     let(:film) { Film.new("Леон", "Люк Бессон", 1994) }
     it "initialize instance variables" do
-      film.name.should eq "Леон"
-      film.director.should eq "Люк Бессон"
-      film.year.should eq 1994
+      expect(film.name).to eq "Леон"
+      expect(film.director).to eq "Люк Бессон"
+      expect(film.year).to eq 1994
     end
   end
 end
